55d846616790b17b918d01190efbb32fb421762b
Sam Fiechter <sam.fiechter@gmail.com>
2022-01-16 08:15:45 -0500

    Update dot-bashrc.sh

--- dot-bashrc.sh
+++ dot-bashrc.sh
@@ -1,7 +1,7 @@
 # ~/.bashrc: executed by bash(1) for every shell
 
 
-. "$(/opt/homebrew/bin/brew shellenv)"
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 export BASH_SILENCE_DEPRECATION_WARNING=1
 
